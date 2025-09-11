dbc44249bde35eaf98fcc7d137e5b422b2e36679
twhit-006 <trevor.whitaker.006@gmail.com>
2025-09-10 21:06:16 -0500

    Was super cool but then Josh yelled at me because he hates fun.

--- refrigerant-properties.py
+++ refrigerant-properties.py
@@ -2,49 +2,48 @@ import CoolProp.CoolProp as CP
 import numpy as np
 import pint
 
-ureg = pint.UnitRegistry()
-Q_ = ureg.Quantity
-
-# Canonical property symbols expected by CoolProp
-# T[K], P[Pa], H[J/kg], S[J/kg/K], D[kg/m^3], V[m^3/kg], Q[-], Cp[J/kg/K], viscosity[Pa.s], conductivity[W/m/K]
-DIMENSIONS = {
-    "T": ureg.kelvin,
-    "P": ureg.pascal,
-    "H": ureg.joule / ureg.kilogram,
-    "S": ureg.joule / (ureg.kilogram * ureg.kelvin),
-    "D": ureg.kilogram / ureg.meter**3,
-    "V": ureg.meter**3 / ureg.kilogram,
-    "Q": ureg.dimensionless,
-    "Cp": ureg.joule / (ureg.kilogram * ureg.kelvin),
-    "viscosity": ureg.pascal * ureg.second,
-    "conductivity": ureg.watt / (ureg.meter * ureg.kelvin)
-}
-
-# Commonly used fluids and corresponding CoolProp names
-FLUIDS = {
-    "1233": "R1233zd(E)",
-    "515": "R1234ze(E)[0.127]&R227ea[0.873]",
-    "PG25": "INCOMP::APG-25%",
-    "water": "Water"
-}
-
 class fluid:
     def __init__(self, name):
-        if name not in FLUIDS:
+        self.ureg = pint.UnitRegistry()
+        self.Q_ = self.ureg.Quantity
+
+        # Canonical property symbols expected by CoolProp
+        # T[K], P[Pa], H[J/kg], S[J/kg/K], D[kg/m^3], Q[-], C[J/kg/K], viscosity[Pa.s], conductivity[W/m/K]
+        self.parameters = {
+            "T": self.ureg.kelvin,
+            "P": self.ureg.pascal,
+            "H": self.ureg.joule / self.ureg.kilogram,
+            "S": self.ureg.joule / (self.ureg.kilogram * self.ureg.kelvin),
+            "D": self.ureg.kilogram / self.ureg.meter**3,
+            "Q": self.ureg.dimensionless,
+            "C": self.ureg.joule / (self.ureg.kilogram * self.ureg.kelvin),
+            "viscosity": self.ureg.pascal * self.ureg.second,
+            "conductivity": self.ureg.watt / (self.ureg.meter * self.ureg.kelvin)
+        }
+
+        # Commonly used fluids and corresponding CoolProp names
+        self.fluids = {
+            "1233": "R1233zd(E)",
+            "515": "R1234ze(E)[0.127]&R227ea[0.873]",
+            "pg25": "INCOMP::APG-25%",
+            "water": "Water"
+        }
+        
+        if name not in self.fluids:
             self.name = name
             print(f"User provided custom fluid, ensure valid CoolProp name: {name}")
         else:
-            self.name = FLUIDS[name]
+            self.name = self.fluids[name]
 
     # Convert to SI units from user input
-    def to_si(prop, value, units):
-        nonsi = Q_(value, units)
-        si = nonsi.to(DIMENSIONS[prop])
+    def to_si(self, prop, value, units):
+        nonsi = self.Q_(value, units)
+        si = nonsi.to(self.parameters[prop])
         return si.magnitude
     
     # Convert from SI units to user input
-    def from_si(prop, value, units):
-        si = Q_(value, DIMENSIONS[prop])
+    def from_si(self, prop, value, units):
+        si = self.Q_(value, self.parameters[prop])
         nonsi = si.to(units)
         return nonsi.magnitude
 
@@ -53,9 +52,9 @@ class fluid:
         if prop1 == prop2:
             raise ValueError("Input properties must be different")
         self.prop1 = prop1
-        self.val1 = fluid.to_si(prop1, value1, units1)
+        self.val1 = self.to_si(prop1, value1, units1)
         self.prop2 = prop2
-        self.val2 = fluid.to_si(prop2, value2, units2)
+        self.val2 = self.to_si(prop2, value2, units2)
 
     # Check if state is valid and input properties are appropriate for the given state
     def state_check(self):
@@ -66,21 +65,18 @@ class fluid:
             raise ValueError("Two independent intensive properties required for two-phase state")
 
     # Convert units, check state, and update all properties
-    def update_properties(self, prop1, value1, units1, prop2, value2, units2):
+    def get_properties(self, prop1, value1, units1, prop2, value2, units2):
         self.val_set(prop1, value1, units1, prop2, value2, units2)
         self.state_check()
         self.prop_array = []
-
+        property_dict = {}
         # Get properties from CoolProp
-        for prp in DIMENSIONS.keys():
-            setattr(self, prp, CP.PropsSI(prp, self.prop1, self.val1, self.prop2, self.val2, self.name))
-            self.prop_array.append(getattr(self, prp))
-
+        for prp in self.parameters.keys():
+            if prp == "Q" and self.phase != "two_phase":
+                continue
+            property_dict[prp] = CP.PropsSI(prp, self.prop1, self.val1, self.prop2, self.val2, self.name)
+        return property_dict
         
 if __name__ == "__main__":
-    r515 = fluid("PG25")
-    r515.update_properties("T", 25, "degC", "P", 150, "psi")
-    print(f"Temperature: {r515.T} {DIMENSIONS['T']}")
-    print(f"Pressure: {r515.P} {DIMENSIONS['P']}")
-    print(f"Thermodynamic state: {r515.phase}")
-    print(f"Property array: {r515.prop_array}")
+    fld = fluid("515")
+    print(fld.get_properties("T", 25, "degC", "P", 150, "psi"))
